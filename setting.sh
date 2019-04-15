e75ed64366ef9fbcde6b94dab3816677e9016517
root <adfsghjalison@gmail.com>
2019-04-15 02:39:26 +0000

    set vim, input, tmux

---
+++ setting.sh
@@ -0,0 +1,20 @@
+# vim 
+if [ ! -a ~/.vimrc ]; then
+    cp vimrc ~/.vimrc
+fi
+
+# tab completion case-insensitive 
+if [ ! -a ~/.inputrc ]; then
+    echo '$include /etc/inputrc' > ~/.inputrc;
+    echo 'set completion-ignore-case On' >> ~/.inputrc
+    . ~/.inputrc
+fi
+
+# tmux 
+sudo apt-get install tmux
+git clone https://github.com/adfsghjalison/.tmux.git
+cp .tmux/.tmux.conf ~/.tmux.conf
+cp .tmux/.tmux.conf.local ~/.tmux.conf.local
+rm -r .tmux
+
+
